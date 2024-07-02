cc1e07e8e46ed5c7c6b0571e3b0e3c9906a5f28b
Simon <73712759+ZvSimon@users.noreply.github.com>
2024-07-02 22:26:48 +0200

    Run JS on HTML file

--- assets/scripts/vendor.js
+++ assets/scripts/vendor.js
@@ -10,4 +10,5 @@ const currentCalculationOutput = document.getElementById('current-calculation');
 function outputResult(result, text) {
   currentResultOutput.textContent = result;
   currentCalculationOutput.textContent = text;
+
 }
