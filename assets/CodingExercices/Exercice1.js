6132211d0b758f67dc6b488cdacd760b89b315df
Simon <73712759+ZvSimon@users.noreply.github.com>
2024-07-02 23:17:28 +0200

    Exercice 1 done

---
+++ assets/CodingExercices/Exercice1.js
@@ -0,0 +1,6 @@
+//Create two variable or constants, a and b, that should store the values 5 and 3 respectively. Thereafter, also add a third variable or constant, sum, that stores the sum of a and b.
+
+const a = 5;
+const b = 3;
+const sum = a + b;
+console.log(sum);
