07290b09aa42a9beb09158f86fbd424c6e0db2b6
Simon <73712759+ZvSimon@users.noreply.github.com>
2024-07-21 17:19:08 +0200

    Adding following step for calculation

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,6 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
+let logEntries = [];
 function getUserNumberInput(){
     return parseInt(userInput.value);
 }
@@ -7,29 +8,43 @@ function createAndWriteOutput(operator,resultBeforeCalc,calcNumber){
     const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
     outputResult(currentResult,calcDescription);
 }
+function writeToLog(operationIdentifier,prevResult,operationNumber,newResult){
+    const logEntry = {
+        operation:operationIdentifier,
+        prevResult: prevResult,
+        number: operationNumber,
+        result: newResult
+    }
+    logEntries.push(logEntry);
+}
 function add(){
     const enteredNumber = getUserNumberInput();
    const initialResult = currentResult;
     currentResult = currentResult + enteredNumber;
     createAndWriteOutput('+',initialResult,enteredNumber);
+    writeToLog('ADD',initialResult,enteredNumber,currentResult);
+    
 }
 function subtract(){
     const enteredNumber = getUserNumberInput();
     const initialResult = currentResult;
     currentResult = currentResult - enteredNumber;
     createAndWriteOutput('-',initialResult,enteredNumber);
+    writeToLog('SUBSTRACT',initialResult,enteredNumber,currentResult);
 }
 function multiply(){
     const enteredNumber = getUserNumberInput();
     const initialResult = currentResult;
     currentResult = currentResult * enteredNumber;
     createAndWriteOutput('*',initialResult,enteredNumber);
+    writeToLog('MULTIPLY',initialResult,enteredNumber,currentResult);
 }
 function divide(){
     const enteredNumber = getUserNumberInput();
     const initialResult = currentResult;
     currentResult = currentResult / enteredNumber;
     createAndWriteOutput('/',initialResult,enteredNumber);
+    writeToLog('DIVIDE',initialResult,enteredNumber,currentResult);
 }
 addBtn.addEventListener('click',add);
 subtractBtn.addEventListener('click',subtract);
